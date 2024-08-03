0bb99870621d5d9126c2b372258ef15c4a013ce6
Hongseob Lee <sjun2918@naver.com>
2024-08-03 19:34:31 +0900

    chore: init registry server

---
+++ registry-server/src/main/kotlin/org/example/registryserver/RegistryServerApplication.kt
@@ -0,0 +1,13 @@
+package org.example.registryserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@EnableEurekaServer
+@SpringBootApplication
+class RegistryServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<RegistryServerApplication>(*args)
+}
