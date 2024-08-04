3af08f6b99eeb6d3df6980afa4eb119123b4a99a
Hongseob Lee <sjun2918@naver.com>
2024-08-04 15:53:12 +0900

    chore: init gateway server

---
+++ gateway-server/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("org.springframework.boot") version "3.2.8"
+    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.spring") version "1.9.24"
+}
+
+group = "org.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2023.0.3"
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway-mvc")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ gateway-server/src/main/kotlin/org/example/gatewayserver/GatewayServerApplication.kt
@@ -0,0 +1,13 @@
+package org.example.gatewayserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+
+@SpringBootApplication
+@EnableDiscoveryClient
+class GatewayServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<GatewayServerApplication>(*args)
+}
