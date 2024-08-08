0cd48eb266d67bbe57627f6f5f0911bf5751951d
leehongseob <hongxeob@wemeetmobility.com>
2024-08-08 17:51:45 +0900

    feat: 대출/반납 카프카 프로듀서 및 이벤트 발행 적용

--- rental/src/main/kotlin/org/example/rental/service/impl/RentalServiceImpl.kt
+++ rental/src/main/kotlin/org/example/rental/service/impl/RentalServiceImpl.kt
@@ -1,5 +1,6 @@
 package org.example.rental.service.impl
 
+import org.example.rental.adaptor.RentalProducer
 import org.example.rental.domain.Rental
 import org.example.rental.exception.RentalException
 import org.example.rental.repository.RentalRepository
@@ -12,8 +13,13 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class RentalServiceImpl(
     private val rentalRepository: RentalRepository,
+    private val rentalProducer: RentalProducer,
 ) : RentalService {
     private val log = LoggerFactory.getLogger(this::class.java)
+
+    companion object {
+        const val RENT_SAVE_POINTS = 1000
+    }
 
     /**
      * 대출
@@ -35,12 +41,13 @@ class RentalServiceImpl(
         rentalRepository.save(rental)
         log.info("=== 대출 로직 완료 후 저장 완료 ===")
 
-        /**
-         * Todo 대출 후 이벤트 전송
-         * 1. 도서 서비스에 재고 감소를 위해 도서 대출 이벤트 발송
-         * 2. 도서 카탈로그 서비스에 대출된 도서로 상태를 변경하기 위한 이벤트 발송
-         * 3. 대출로 인한 사용자 포인트 적립을 위해 사용자 서비스에 이벤트 발송
-         * */
+        // 대출 후 이벤트 전송
+        // 1. 도서 서비스에 재고 감소를 위해 도서 대출 이벤트 발송
+        rentalProducer.updateBookStatus(bookId, "UNAVAILABLE")
+        // 2. 도서 카탈로그 서비스에 대출된 도서로 상태를 변경하기 위한 이벤트 발송
+        rentalProducer.updateBookCatalogStatus(bookId, "RENT_BOOK")
+        // 3. 대출로 인한 사용자 포인트 적립을 위해 사용자 서비스에 이벤트 발송
+        rentalProducer.savePoints(userId, RENT_SAVE_POINTS)
 
         return rental
     }
@@ -55,11 +62,12 @@ class RentalServiceImpl(
         rental.returnBook(bookId)
         rentalRepository.save(rental)
 
-        /**
-         * Todo 반납 후 이벤트 전송
-         * 1. 도서 서비스에 재고 증가를 위해 도서 대출 이벤트 발송
-         * 2. 도서 카탈로그 서비스에 반납된 도서로 상태를 변경하기 위한 이벤트 발송
-         * */
+        // 반납 후 이벤트 전송
+        // 1. 도서 서비스에 재고 증가를 위해 도서 대출 이벤트 발송
+        rentalProducer.updateBookStatus(bookId, "AVAILABLE")
+        // 도서 카탈로그 서비스에 반납된 도서로 상태를 변경하기 위한 이벤트 발송
+        rentalProducer.updateBookCatalogStatus(bookId, "RETURN_BOOK")
+
         return rental
     }
 
