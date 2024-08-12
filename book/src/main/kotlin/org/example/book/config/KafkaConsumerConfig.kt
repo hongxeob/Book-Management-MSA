5a6ef737ac5cae34ec1058397ede7e7304b5db8a
hongxeob <sjun2918@naver.com>
2024-08-12 10:27:13 +0900

    feat: Book Consumer Config 구현 (Spring Apache Kafka)

---
+++ book/src/main/kotlin/org/example/book/config/KafkaConsumerConfig.kt
@@ -0,0 +1,41 @@
+package org.example.book.config
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+
+@Configuration
+@EnableKafka
+class KafkaConsumerConfig(
+    private val kafkaProperties: KafkaProperties,
+) {
+    companion object {
+        private const val GROUP_ID = "group_id"
+    }
+
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String, String> {
+        val props =
+            mapOf(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to kafkaProperties.bootstrapServers,
+                ConsumerConfig.GROUP_ID_CONFIG to GROUP_ID,
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+            )
+        return DefaultKafkaConsumerFactory(props)
+    }
+
+    @Bean
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, String>()
+        factory.consumerFactory = consumerFactory()
+
+        return factory
+    }
+}
